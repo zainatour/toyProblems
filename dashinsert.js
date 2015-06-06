04af4f51f11df47c0050e671d6c6ddcda93435df
Zaina Natour <zainanatour@gmail.com>
2015-06-06 15:00:10 -0700

    my first toy problem

---
+++ dashinsert.js
@@ -0,0 +1,20 @@
+function dashInsert (count){
+
+var string = count.toString();
+
+var output = "";
+
+
+for (var i = 0; i < string.length; i++) {
+	if (string[i] % 2 != 0)
+		output = output + (string[i].concat("-"))
+	else output = output + (string[i])
+};
+
+return output;
+
+}
+
+console.log(dashInsert(70982));
+
+
