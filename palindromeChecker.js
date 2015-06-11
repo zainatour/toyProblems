34190aa47d2b321b428d3cbfcae452c708965257
Noah Sluss <slussnoah@gmail.com>
2015-06-11 10:18:27 -0700

    add palindromeChecker problem

---
+++ palindromeChecker.js
@@ -0,0 +1,10 @@
+//Write a function to determine if a given string is a palindrome.
+//eg:
+//isPalindrome("racecar")
+//=> True
+//isPalindrome("hello")
+//=> False
+function isPalindrome (word) {
+    //code here
+    return result;
+}
