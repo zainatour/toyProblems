662572a83e5076445cde20a32b83451fc8da8687
Noah Sluss <slussnoah@gmail.com>
2015-06-12 09:58:19 -0700

    add sumOfSquares problem

---
+++ sumOfSquares.js
@@ -0,0 +1,8 @@
+//given a list of integers, find the sum of squares of those integers.
+//eg:
+// sumOfSquares([1,2,3])
+// => 14
+function sumOfSquares (numbers) {
+    //your code
+    return result;
+}
