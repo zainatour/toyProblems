8049e0cd6d7dcaf85057b5da4380f953a7f51b28
Noah Sluss <slussnoah@gmail.com>
2015-06-13 14:27:03 -0700

    add forEach prompt

---
+++ forEach.js
@@ -0,0 +1,13 @@
+//Arrays have a native method called forEach: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+//Write a function that acts like the native method
+//eg:
+//forEach ([1,2,3], function (x) {
+//  console.log(x);
+//})
+//>1
+//>2
+//>3
+
+function forEach (list, fn) {
+    //code here
+}
