08567987b28f9db74b66fb1aafbe7949f9516340
Zaina Natour <zainanatour@gmail.com>
2015-06-17 15:54:29 -0700

    it works!

--- onlyEvens.js
+++ onlyEvens.js
@@ -3,7 +3,20 @@
 // eg:
 // onlyEvens([1,2,3,4,8,7,6,5])
 // => [2,4,8,6]
+
 function onlyEvens (list) {
-    //code here
-    return result;
+
+	var i = 0;
+	var evens = []
+  
+    while (i < list.length) {
+    	if(list[i]%2 === 0) {
+    		evens.push(list[i]);
+    	} 	
+    	i++
+    }
+
+    return evens;
 }
+
+console.log(onlyEvens([1,2,3,4,5,6,7,8,9,11,12,14,16,14,15,16]))
