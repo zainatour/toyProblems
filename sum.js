70d6ead76bba33f525a2310b0f1ab92a17b31440
Noah Sluss <slussnoah@gmail.com>
2015-06-18 11:28:50 -0700

    add sum prompt

---
+++ sum.js
@@ -0,0 +1,7 @@
+// Write a function, sum(x1,...,xn), that takes any number of arguments and
+// returns the sum of those arguments. Write this function without using any
+// loops (Hint: try using reduce)
+
+var sum = function() {
+    //code
+}
